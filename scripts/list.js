8fc30cd515b48fffbcbd776344903e2828a7d7aa
Soumya Ranjan Nayak <soumyarj904@gmail.com>
2024-10-10 21:21:12 +0530

    Checkpoint 8

--- scripts/list.js
+++ scripts/list.js
@@ -3,3 +3,19 @@ document.querySelectorAll(".hotel-item")[0].addEventListener("click", (e) => {
 
   window.location.href = "./detail.html";
 });
+
+document.getElementById("list-view-tab").addEventListener("click", function () {
+  document.querySelectorAll(".nav-link").forEach((link) => link.classList.remove("active"));
+  this.querySelector(".nav-link").classList.add("active");
+
+  document.getElementById("hotel-view").style.display = "flex";
+  document.getElementById("map-view").style.display = "none";
+});
+
+document.getElementById("map-view-tab").addEventListener("click", function () {
+  document.querySelectorAll(".nav-link").forEach((link) => link.classList.remove("active"));
+  this.querySelector(".nav-link").classList.add("active");
+
+  document.getElementById("hotel-view").style.display = "none";
+  document.getElementById("map-view").style.display = "block";
+});
